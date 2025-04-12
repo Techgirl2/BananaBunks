394794ec3a812f0729b4b89b2b893ef73d508af2
Angelina Cruz <angelinacruz465@gmail.com>
2025-04-12 03:15:23 -0700

    now running on expo go

--- App.tsx
+++ App.tsx
@@ -4,20 +4,17 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from './src/screens/LoginScreen';
 import HomeScreen from './src/screens/HomeScreen';
 import { StatusBar } from 'expo-status-bar';
-import { TailwindProvider } from 'nativewind';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <TailwindProvider>
-      <NavigationContainer>
-        <StatusBar style="auto" />
-        <Stack.Navigator initialRouteName="Login">
-          <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
-          <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </TailwindProvider>
+    <NavigationContainer>
+      <StatusBar style="auto" />
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
